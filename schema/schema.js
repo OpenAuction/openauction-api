7d756296e325a3d899dd0d83d8ae2b5679ef8ae6
Zachary Friss <zfriss@hubspot.com>
2019-10-21 02:10:34 -0400

    Fix schema, setup apollo manager

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,6 @@
+const { gql } = require('apollo-server');
 const fs = require('fs');
-
-const schema = fs.readFileSync('./schema.graphql');
+const schema = fs.readFileSync('./schema/schema.graphql');
 
 module.exports = gql`
   ${schema}
